7a60171d4023d180653338efca9e864dd0e31938
berwin <berwin1995@qq.com>
2019-05-16 19:39:15 +0800

    🚧 Update Parser

--- src/compose.js
+++ src/compose.js
@@ -4,6 +4,23 @@ class Compose {
     this.properties = properties;
   }
 
+  /*
+   * {
+   *   gridTemplateColumns: ['auto', '1fr'],
+   *   gridTemplateRows: ['auto', '1fr', 'auto']
+   * }
+   * To:
+   * {
+   *   gridTemplateColumns: ['200', '800'],
+   *   gridTemplateRows: ['150', '800', '50']
+   * }
+   * To:
+   * [
+   *   [{width: '200', height: '150', name: '?'}, {width: '800', height: '150', name: '?'}],
+   *   [{width: '200', height: '800', name: '?'}, {width: '800', height: '800', name: '?'}],
+   *   [{width: '200', height: '50', name: '?'}, {width: '800', height: '50', name: '?'}]
+   * ]
+   */
   compose() {
     return {
       width: 0,
--- src/parser.js
+++ src/parser.js
@@ -1,25 +1,93 @@
+import {isDef} from './util';
+
 class Parser {
   constructor(node, properties) {
     this.node = node;
     this.properties = properties;
   }
 
-  parse() {}
+  /*
+   * Parse Grid Properties, Like this:
+   * {
+   *   grid-template-columns: auto 1fr;
+   *   grid-template-rows:auto 1fr auto;
+   * }
+   * To:
+   * {
+   *   gridTemplateColumns: ['auto', '1fr'],
+   *   gridTemplateRows: ['auto', '1fr', 'auto']
+   * }
+   */
+  parse() {
+    const properties = {
+      ...this.parsePublicProperties(this.properties),
+      ...this.parseGridContainer(this.properties),
+      ...this.parseGridItem(this.properties),
+    };
+    return Object.fromEntries(
+      Object.entries(properties)
+        .filter(item => isDef(item[1]))
+    );
+  }
+
+  parsePublicProperties(properties) {
+    return {
+      width: properties.width,
+      height: properties.height,
+    };
+  }
 
   /*
-   * Parser Grid container
-   * @retrun Grid Matrix
+   * Parser Grid Container properties
    */
-  parseGridContainer() {}
+  parseGridContainer(properties) {
+    return {
+      gridTemplateRows: this.parseGridTemplateRows(properties),
+      gridTemplateColumns: this.parseGridTemplateColumns(properties),
+    };
+  }
+
+  parseGridTemplateRows(properties) {
+    const gridTemplateRows = properties.gridTemplateRows && properties.gridTemplateRows.trim();
+    return gridTemplateRows
+      ? gridTemplateRows.split(' ')
+      : undefined;
+  }
+
+  parseGridTemplateColumns(properties) {
+    const gridTemplateColumns = properties.gridTemplateColumns && properties.gridTemplateColumns.trim();
+    return gridTemplateColumns
+      ? gridTemplateColumns.split(' ')
+      : undefined;
+  }
 
   /*
-   * Parser Grid item
+   * Parser Grid item properties
    */
-  parseGridItem() {}
+  parseGridItem(properties) {
+    return {
+      gridColumn: this.parseGridColumn(properties),
+      gridRow: this.parseGridRow(properties),
+    };
+  }
 
-  parseGridTemplateRows() {}
+  parseGridColumn(properties) {
+    const columnSpan = 1; // grid span default is 1
+    return {
+      gridColumnStart: 1,
+      gridColumnEnd: 1,
+      columnSpan,
+    };
+  }
 
-  parseGridTemplateColumns() {}
+  parseGridRow(properties) {
+    const rowSpan = 1; // grid span default is 1
+    return {
+      gridRowStart: 1,
+      griRowEnd: 1,
+      rowSpan,
+    };
+  }
 }
 
 export default Parser;
